55e2ef200a8c9e1d55e19ff37c5cf2abdc7bca72
ImGabreuw <imgabreuw.dev@gmail.com>
2021-04-04 11:02:38 -0300

    Entendendo variáveis

---
+++ douglas-motta/entendendo-variaveis/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    /*
+    val
+     */
+    val nome1: String = "Gabriel"
+    // nome = "Felipe" Error: val não pode ser reassinado
+
+    val nome2: String
+    nome2 = "Gabriel"
+    // nome2 = "Felipe" Error: val não pode ser reassinado
+
+    /*
+    var
+     */
+    var idade1: Int = 16
+    idade1 = 17
+
+    var idade2: Int
+    idade2 = 16
+    idade2 = 17
+}

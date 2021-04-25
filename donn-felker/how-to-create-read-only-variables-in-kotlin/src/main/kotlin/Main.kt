0903e267b180ef2ca4cf8d599f65f387264c1bdc
ImGabreuw <imgabreuw.dev@gmail.com>
2021-04-25 18:48:34 -0300

    How to Create Read-Only Variables in Kotlin

---
+++ donn-felker/how-to-create-read-only-variables-in-kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val fullName = "Donn Falker"
+    println(fullName)
+
+//    fullName = "John Falker" Val não pode ser reatribuído
+
+    val age = 32
+    println(age)
+
+//    age = 42 Val não pode ser reatribuído
+}

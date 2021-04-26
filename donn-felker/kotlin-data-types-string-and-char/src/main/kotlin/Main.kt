44daf4b9beb9688234aa17dc75bed48b98153523
ImGabreuw <imgabreuw.dev@gmail.com>
2021-04-26 17:36:03 -0300

    Kotlin Data Types: String and Char

---
+++ donn-felker/kotlin-data-types-string-and-char/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val helloWorld: String = "Hello World"
+    println(helloWorld.length)
+    println(helloWorld.decapitalize())
+    println(helloWorld.toLowerCase())
+
+    val firstName = "Hello"
+
+    val c: Char = 'x' // 16-bit Unicode Character
+
+    var foo: String = "y"
+}

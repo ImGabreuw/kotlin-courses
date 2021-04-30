de659b813d896f1bcaf9dea7197f4e1db6d7f3fc
ImGabreuw <imgabreuw.dev@gmail.com>
2021-04-30 17:42:46 -0300

    How to Use the Kotlin Single Line If/Else Statement

---
+++ donn-felker/how-to-use-the-kotlin-single-line-IfElse-statement/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val age = 11
+
+//    if (age > 10) {
+//        println("The age is greater than 10")
+//    }
+//    or
+//    if (age > 10) println("The age is greater than 10")
+    if (age > 10) println("The age is greater than 10") else println("The age is less than 10")
+
+    println("Hello there")
+}

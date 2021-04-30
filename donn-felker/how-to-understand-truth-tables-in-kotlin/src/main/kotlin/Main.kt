fa1dfafde569cdc7b1a83b702815528f14b63ed9
ImGabreuw <imgabreuw.dev@gmail.com>
2021-04-30 18:00:08 -0300

    How to Understand Truth Tables in Kotlin

---
+++ donn-felker/how-to-understand-truth-tables-in-kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val myAge = 32
+    val yourAge = 32
+
+    val myName = "Donn"
+    val yourName = "Tusher"
+
+    if ((myAge != yourAge) && (myName != yourName)) {
+        println("Both items are true")
+    }
+}

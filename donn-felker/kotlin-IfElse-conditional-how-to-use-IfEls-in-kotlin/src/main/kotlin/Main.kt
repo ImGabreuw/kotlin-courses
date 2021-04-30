83832c5d706009907f3ba6e31b4c3ad747de731c
ImGabreuw <imgabreuw.dev@gmail.com>
2021-04-30 17:31:07 -0300

    Kotlin If/Else Conditional - How to use If/Else in Kotlin

---
+++ donn-felker/kotlin-IfElse-conditional-how-to-use-IfEls-in-kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val calories = 2000
+
+    if (calories > 2000) {
+        println("You´ve consumed all your calories for the day")
+    } else if (calories > 1500) {
+        println("You have a few more calories left to eat")
+    } else {
+        println("You still have some calories left")
+    }
+}

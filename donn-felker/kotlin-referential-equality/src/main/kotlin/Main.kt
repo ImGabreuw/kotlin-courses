8109b483188e7e8e12008031da51c7fe97d63732
ImGabreuw <imgabreuw.dev@gmail.com>
2021-05-01 12:53:06 -0300

    Kotlin Referential Equality

--- donn-felker/kotlin-referential-equality/src/main/kotlin/Main.kt
+++ donn-felker/kotlin-referential-equality/src/main/kotlin/Main.kt
@@ -1,2 +1,29 @@
-class Main {
+fun main() {
+    example1()
+    example2()
+    example3()
 }
+
+fun example1() {
+    val a = 12
+    val b = 12
+
+    println(a === b)
+    println(a == b)
+}
+
+fun example2() {
+    val person1 = Person("Donn")
+    val person2 = Person("Donn")
+
+    println(person1 === person2)
+}
+
+fun example3() {
+    val person1 = Person("Donn")
+    val person2 = Person("Donn")
+
+    println(person1 !== person2)
+}
+
+class Person(val name: String)

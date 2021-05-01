f0467b95ca9e8a2fd85508d2be671f0bfe2b335c
ImGabreuw <imgabreuw.dev@gmail.com>
2021-05-01 12:33:23 -0300

    Kotlin Structural Equality

---
+++ donn-felker/kotlin-structural-equality/src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main() {
+    example1()
+    example2()
+    example3()
+}
+
+fun example1() {
+    // Example 1
+    val name1 = "Donn"
+    val name2 = "Tushar"
+
+//    println(name1.equals(name2))
+    println(name1 == name2)
+}
+
+fun example2() {
+    // Example 2
+    val name1 = "Donn"
+    val name2 = "Donn"
+
+    println(name1 == name2)
+}
+
+fun example3() {
+    // Example 3
+    val name1 = "Donn"
+    val name2 = "Donn"
+
+    println(name1 != name2)
+}

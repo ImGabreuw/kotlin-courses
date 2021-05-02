b753d703d72baf1769dfeb2e3fca6c440777160c
ImGabreuw <imgabreuw.dev@gmail.com>
2021-05-02 11:37:50 -0300

    How to Create a Function in Kotlin

---
+++ donn-felker/how-to-create-a-function-in-kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    helloWorld()
+
+    purpleCow()
+    purpleCow()
+    purpleCow()
+    purpleCow()
+}
+
+fun helloWorld() {
+    println("Hello World")
+}
+
+fun purpleCow() {
+    println("Hello Purple Cow")
+    moo()
+    println("Hello Blue Cow")
+    moo()
+}
+
+fun moo() {
+    println("Moo!")
+    println("Moo Moo Buckroo!")
+}
